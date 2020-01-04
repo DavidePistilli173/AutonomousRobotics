a1b4a13e29be9922423e56b168d8b7a79bd4d505
DavidePistilli173 <davidepistilli173@gmail.com>
2020-01-04 13:09:37 +0100

    Basic manipulator movement

--- hw2/src/Task.cpp
+++ hw2/src/Task.cpp
@@ -6,6 +6,9 @@
 
 #include <moveit_visual_tools/moveit_visual_tools.h>
 #include <ros/ros.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <tf2_ros/transform_listener.h>
 
 #include "Task.hpp"
 
@@ -28,10 +31,84 @@ void Task::run()
 {
     ros::NodeHandle n;
     ros::Subscriber poses = n.subscribe(POSES_TOPIC, Q_LEN, _moveManipulator);
-    
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
     while (ros::ok())
     {
-        ros::spinOnce();
+        moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+        moveit::planning_interface::PlanningSceneInterface planninc_scene_interface;
+        const robot_state::JointModelGroup* joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+        // We can print the name of the reference frame for this robot.
+        ROS_INFO("Reference frame: %s", move_group.getPlanningFrame().c_str());
+
+        // We can also print the name of the end-effector link for this group.
+        ROS_INFO("End effector link: %s", move_group.getEndEffectorLink().c_str());
+
+        for (const auto& target : _targets)
+        {
+            moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
+            std::vector<double> joint_group_positions;
+            current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+            joint_group_positions[0] = 1.50301;  // radians
+            joint_group_positions[1] = -1.79556;
+            joint_group_positions[2] = -0.849335;
+            joint_group_positions[3] = -2.0492;
+            joint_group_positions[4] = 1.60192;
+            joint_group_positions[5] = 1.54684;
+            move_group.setJointValueTarget(joint_group_positions);
+
+            /*
+            for (const auto& joint : move_group.getCurrentJointValues())
+            {
+                std::cout << joint << std::endl;
+            }
+            */
+            //std::cout << move_group.getCurrentJointValues() << std::endl;
+            //move_group.setPlannerId("RRTStar");
+            
+            moveit::planning_interface::MoveGroupInterface::Plan myPlan;
+            bool success = (move_group.plan(myPlan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+            if (!success)
+            {
+                ROS_WARN("Planning to reference pose failed.");
+            }
+            move_group.execute(myPlan);
+
+            geometry_msgs::Pose target_pose;
+            geometry_msgs::Pose transformedTargetPose;
+            geometry_msgs::TransformStamped transformStamped;
+            transformStamped = tfBuffer.lookupTransform("camera_rgb_optical_frame", "world", ros::Time(0));
+
+            //target_pose.orientation.w = target.rotation.w;
+            //target_pose.orientation.x = target.rotation.x;
+            //target_pose.orientation.y = target.rotation.y;
+            //target_pose.orientation.z = target.rotation.z;
+            target_pose.position.x = target.coordinates.x;
+            target_pose.position.y = target.coordinates.y;
+            target_pose.position.z = target.coordinates.z;
+            geometry_msgs::PoseStamped currentPose = move_group.getCurrentPose();
+
+            tf2::doTransform(target_pose, transformedTargetPose, transformStamped);
+            transformedTargetPose.position.z += 0.3;
+            transformedTargetPose.position.x += 0.1;
+            transformedTargetPose.orientation.w = currentPose.pose.orientation.w;
+            transformedTargetPose.orientation.x = currentPose.pose.orientation.x;
+            transformedTargetPose.orientation.y = currentPose.pose.orientation.y;
+            transformedTargetPose.orientation.z = currentPose.pose.orientation.z;
+
+            move_group.setPoseTarget(transformedTargetPose);
+            success = (move_group.plan(myPlan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+            if (!success)
+            {
+                ROS_WARN("Planning to reference pose failed.");
+            }
+            move_group.execute(myPlan);
+        }
     }
 }
 
@@ -75,19 +152,4 @@ void Task::_moveManipulator(const hw1::poseArray::ConstPtr &msg)
             _targets.push_back(newObject);
         }
     }
-
-    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-    moveit::planning_interface::PlanningSceneInterface planninc_scene_interface;
-    const robot_state::JointModelGroup* _joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-
-    // We can print the name of the reference frame for this robot.
-    ROS_INFO("Reference frame: %s", move_group.getPlanningFrame().c_str());
-
-    // We can also print the name of the end-effector link for this group.
-    ROS_INFO("End effector link: %s", move_group.getEndEffectorLink().c_str());
-
-
-    geometry_msgs::Pose reference_pose;
-    reference_pose.orientation.w = 1.0;
-    //reference_pose.position.x = 
 }
